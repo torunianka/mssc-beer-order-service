cb096996e5dc0d20e44dfbacc317cf76e0242eb6
torunianka <kstawikowska@gmail.com>
2021-09-06 22:54:44 +0200

    BeerDto, Decorator

--- src/main/java/guru/sfg/beer/order/service/web/model/BeerOrderLineDto.java
+++ src/main/java/guru/sfg/beer/order/service/web/model/BeerOrderLineDto.java
@@ -22,6 +22,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -32,16 +33,20 @@ public class BeerOrderLineDto extends BaseItem {
 
     @Builder
     public BeerOrderLineDto(UUID id, Integer version, OffsetDateTime createdDate, OffsetDateTime lastModifiedDate,
-                            String upc, String beerName, UUID beerId, Integer orderQuantity) {
+                            String upc, String beerName, UUID beerId, Integer orderQuantity, BigDecimal price, String beerStyle) {
         super(id, version, createdDate, lastModifiedDate);
         this.upc = upc;
         this.beerName = beerName;
         this.beerId = beerId;
         this.orderQuantity = orderQuantity;
+        this.price = price;
+        this.beerStyle = beerStyle;
     }
 
     private String upc;
     private String beerName;
     private UUID beerId;
     private Integer orderQuantity = 0;
+    private BigDecimal price;
+    private String beerStyle;
 }
